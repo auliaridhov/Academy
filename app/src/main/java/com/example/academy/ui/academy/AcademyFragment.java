8cb302e2d187fdc2e4ce751cf6c5700f62aece98
auliaridhov <auliaridhoverdika@gmail.com>
2020-11-10 18:45:59 +0700

    Cek commit

--- app/src/main/java/com/example/academy/ui/academy/AcademyFragment.java
+++ app/src/main/java/com/example/academy/ui/academy/AcademyFragment.java
@@ -24,6 +24,8 @@ public class AcademyFragment extends Fragment {
     private RecyclerView rvCourse;
     private ProgressBar progressBar;
 
+
+    //test github
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
         return inflater.inflate(R.layout.fragment_academy, container, false);
